08a0843ba05b7e2ae4beb7ed9d61f60658202911
AlSirang <uddinbashir1999@gmail.com>
2023-02-11 21:48:36 +0500

    update hardhat configs

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,28 @@
 import { HardhatUserConfig } from "hardhat/config";
+import * as dotenv from "dotenv";
 import "@nomicfoundation/hardhat-toolbox";
-
+dotenv.config({});
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+
+  networks: {
+    goerli: {
+      url: process.env.GOERLI_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+
+  etherscan: {
+    apiKey: {
+      goerli: process.env.ETHERSCAN_API_KEY || "",
+    },
+  },
 };
 
 export default config;
